5d5ea1857014519d7efa4d7ffac937436f781449
Zac Murphy <33558810+zwmurph@users.noreply.github.com>
2022-02-23 19:14:06 +0000

    Swtich from String object to literal

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 // Converts an array of objects to a lookup of the same objects
-export function getLookupFromArray<T extends { name: String }>(array: T[]): LookupObject<T> {
+export function getLookupFromArray<T extends { name: string }>(array: T[]): LookupObject<T> {
     return array.reduce((map, obj) => {
-        map[obj.name.toString()] = obj;
+        map[obj.name] = obj;
         return map;
     }, {} as LookupObject<T>);
 }
