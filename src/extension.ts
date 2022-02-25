34e8fd12e4a64f916b32f52dbb0144ed155b76fc
Zac Murphy <33558810+zwmurph@users.noreply.github.com>
2022-02-25 14:47:17 +0000

    Basic webview setup

--- src/extension.ts
+++ src/extension.ts
@@ -2,31 +2,56 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { join } from 'path';
 import { ProjectElements } from './projectSymbols';
+import { getWebviewContent } from './utils';
 
+// Event handler for extension activation
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension is active');
+
+	// Track for active Webview panel
+	let activePanel: vscode.WebviewPanel | undefined = undefined;
 
 	const diagramCmd = vscode.commands.registerCommand('ng-project-diagram.diagram', () => {
 		// Get workspace root and check presence
 		const root = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 		if (root) {
+			// Find tsconfig file
 			const tsconfigPath = join(root, 'tsconfig.json');
 			if (fs.existsSync(tsconfigPath)) {
 				const projectElements = new ProjectElements(tsconfigPath);
 				projectElements.resolveAllWorkspaceSymbols();
-				console.log('modules', projectElements.modules);
-				console.log('components', projectElements.components);
-				console.log('injectables', projectElements.injectables);
 
-				console.log('modules lookup', projectElements.getWorkspaceSymbolLookup('module'));
+				// console.log('modules', projectElements.modules);
+				// console.log('components', projectElements.components);
+				// console.log('injectables', projectElements.injectables);
+				// console.log('modules lookup', projectElements.getWorkspaceSymbolLookup('module'));
+
+				// Show the panel if it already exists, else create a new panel
+				if (activePanel) {
+					activePanel.reveal(activePanel.viewColumn);
+				} else {
+					// Create a webview panel in view column one and set its content
+					activePanel = vscode.window.createWebviewPanel('diagramUI', 'NG Project Diagram', vscode.ViewColumn.One, {
+						localResourceRoots: [] // Pass in an empty array to disallow access to any local resources
+					});
+					activePanel.webview.html = getWebviewContent();
+				}
+
+				// Event handler for when the panel is closed
+				activePanel.onDidDispose(() => {
+					activePanel = undefined;
+				}, null, context.subscriptions);
 			} else {
 				vscode.window.showErrorMessage('tsconfig.json cannot be found');
 			}
+		} else {
+			console.log('no root');
 		}
 	});
 
+	// Add above defined command to the extension context
 	context.subscriptions.push(diagramCmd);
 }
 
-// this method is called when your extension is deactivated
+// Event handler for extension deactivation
 export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,24 @@ export function getLookupFromArray<T extends { name: string }>(array: T[]): Look
     }
 }
 
+// Returns HTML content for the Webview to render
+export function getWebviewContent(): string {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
+    `;
+}
+
 export type LookupObject<T> = {
     [key: string]: T;
 };
