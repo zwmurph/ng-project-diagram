630c8cd02962f1ab0b2715cff4bce3bfab43523a
Zac Murphy <33558810+zwmurph@users.noreply.github.com>
2022-03-09 15:45:34 +0000

    Updated reference to file

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { join } from 'path';
-import { ProjectElements } from './projectSymbols';
+import { ProjectElements } from './projectElements';
 import { DiagramPanel } from './diagramPanel';
 
 // Event handler for extension activation
