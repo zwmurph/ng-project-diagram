89e439371c11d674160855e7181efdeb54ca8b7a
Zac Murphy <33558810+zwmurph@users.noreply.github.com>
2022-03-23 12:45:06 +0000

    Change button text

--- src/diagramPanel.ts
+++ src/diagramPanel.ts
@@ -260,7 +260,7 @@ export class DiagramPanel {
                         </button>
                         <button id="reset-btn" class="icon-btn">
                             <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
-                            <span>Reset Layout</span>
+                            <span>Reset</span>
                         </button>
                         <button id="sync-btn" class="icon-btn">
                             <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" x="0"/></g><g><g><polygon points="7.41,13.41 6,12 2,16 6,20 7.41,18.59 5.83,17 21,17 21,15 5.83,15"/><polygon points="16.59,10.59 18,12 22,8 18,4 16.59,5.41 18.17,7 3,7 3,9 18.17,9"/></g></g></svg>
